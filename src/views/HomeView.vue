c6bb7b73092f55c3b001920b283f3d61d6259a97
Marc Klose <marcklose99@gmail.com>
2023-05-09 08:33:58 +0200

    [ADD] Header and recipegrid component to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import RecipeGrid from "@/components/RecipeGrid.vue";
+import HomeViewHeader from "@/components/HomeViewHeader.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomeViewHeader />
+
+    <div class="container recipes">
+      <h2>Selber hinzugefügte Rezepte</h2>
+      <RecipeGrid/>
+    </div>
   </main>
 </template>
+
+<style lang="scss">
+.container.recipes {
+  padding-top: 100px;
+
+  h2 {
+    width: 100%;
+    text-align: left;
+    padding-left: 20px;
+  }
+}
+</style>
