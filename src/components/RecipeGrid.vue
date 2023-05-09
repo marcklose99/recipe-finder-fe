97d1db62ca3b843bcfd50cf86b7e04dc6b5a2aa9
Marc Klose <marcklose99@gmail.com>
2023-05-09 08:31:15 +0200

    [ADD] RecipeGrid component

---
+++ src/components/RecipeGrid.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Recipe from "@/components/Recipe.vue";
+import {useRecipeStore} from "@/stores/RecipeStore";
+
+const recipeStore = useRecipeStore();
+</script>
+
+<template>
+<div class="recipe-grid">
+  <Recipe
+    v-for="(recipe, index) in recipeStore.filteredRecipes"
+    :key="index"
+    :recipe-data="recipe"/>
+</div>
+</template>
+
+<style lang="scss">
+.recipe-grid {
+  width: 100%;
+  padding: 20px;
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: auto;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+}
+</style>
