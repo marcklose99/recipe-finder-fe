01ef293ad4a586a8d6aa104721b4d2ccc9778c82
Marc Klose <marcklose99@gmail.com>
2023-06-14 10:03:41 +0200

    [ADD] Instructions to recipe

--- src/data/IRecipe.ts
+++ src/data/IRecipe.ts
@@ -1,7 +1,9 @@
-import type {IIngredient} from "@/data/IIngredient";
+import type { IIngredient } from "@/data/IIngredient";
+import type { IInstruction } from "./IInstruction";
 
 export interface IRecipe {
-    id: number | null
-    title: string
-    ingredientList: IIngredient[]
+  id: number | null;
+  title: string;
+  ingredientList: IIngredient[];
+  instructionList: IInstruction[];
 }
