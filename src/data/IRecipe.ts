416ffab921f2bf5b9fbf2c36fe0899c6ffe2839e
Marc Klose <marcklose99@gmail.com>
2023-06-16 09:29:01 +0200

    [ADD] Image name property

--- src/data/IRecipe.ts
+++ src/data/IRecipe.ts
@@ -2,8 +2,9 @@ import type { IIngredient } from "@/data/IIngredient";
 import type { IInstruction } from "./IInstruction";
 
 export interface IRecipe {
-  id: number | null;
-  title: string;
-  ingredientList: IIngredient[];
-  instructionList: IInstruction[];
+  id: number | null
+  title: string
+  ingredientList: IIngredient[]
+  instructionList: IInstruction[]
+  imageName: string
 }
