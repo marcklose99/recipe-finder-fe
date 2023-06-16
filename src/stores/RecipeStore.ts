daf6c2ead8788f5612043d199af91a76e48de4b0
Marc Klose <marcklose99@gmail.com>
2023-06-16 15:55:20 +0200

    [ADD] Watcher for allRecipes array

--- src/stores/RecipeStore.ts
+++ src/stores/RecipeStore.ts
@@ -1,12 +1,19 @@
 import type {Ref} from 'vue'
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
 import {defineStore} from 'pinia'
 import type {IRecipe} from "@/data/IRecipe";
+import { usePopupStore } from './PopupStore';
 
 export const useRecipeStore = defineStore('recipeStore', () => {
+  const popupStore = usePopupStore();
   const allRecipes: Ref<IRecipe[]> = ref([]);
   const filteredRecipes: Ref<IRecipe[]> = ref([]);
 
+  // set counter to zero if a new recipe was added
+  watch(allRecipes, () => {
+    popupStore.counter = 0;
+  });
+  
   function sortRecipe(searchVal: string): void {
     
     if(searchVal != "") {
