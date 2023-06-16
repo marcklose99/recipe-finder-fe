c3ca34ba5d4baf172c2dc71e18a55eb5ea012798
Marc Klose <marcklose99@gmail.com>
2023-06-16 15:49:48 +0200

    [ADD] Method for fetching images from db

--- src/services/FetchService.ts
+++ src/services/FetchService.ts
@@ -1,26 +1,38 @@
 import axios from "axios";
-import type {AxiosResponse} from "axios";
-import type {IRecipe} from "@/data/IRecipe";
+import type { AxiosResponse } from "axios";
+import type { IRecipe } from "@/data/IRecipe";
 
 export default {
-    /**
-     *
-     *
-     * @return Promise<IRecipe[]>
-     */
-    async fetchMatches(): Promise<IRecipe[]>{
-        return axios.get("http://localhost:8080/recipes")
-            .then((response: AxiosResponse) => {
-                return response.data
-            })
-            .catch((error) => {
-                switch (error.response.status) {
-                    default:
-                        console.log(
-                            'FetchService.ts no status case ' + error.response.status
-                        )
-                        break;
-                }
-            })
-    },
-}
+  async fetchMatches(): Promise<IRecipe[]> {
+    return axios
+      .get("http://localhost:8080/recipes")
+      .then((response: AxiosResponse) => {
+        return response.data;
+      })
+      .catch((error) => {
+        switch (error.response.status) {
+          default:
+            console.log(
+              "FetchService.ts no status case " + error.response.status
+            );
+            break;
+        }
+      });
+  },
+  async fetchImage(id: string): Promise<string> {
+    return await axios
+      .get(`http://localhost:8080/recipes/${id}/image`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        switch (error.response.status) {
+          default:
+            console.log(
+              "FetchService.ts no status case " + error.response.status
+            );
+            break;
+        }
+      });
+  },
+};
