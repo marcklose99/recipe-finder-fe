24692def16607277874f66bd11ada0dabb4a4acd
Marc Klose <marcklose99@gmail.com>
2023-06-24 09:03:54 +0200

    [ADD] Overview that displays the recipe grid and search bar

---
+++ src/components/RecipeOverview.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import RecipeGrid from "@/components/RecipeGrid.vue";
+import HomeViewHeader from "@/components/HomeViewHeader.vue";
+</script>
+<template>
+  <div class="content-wrapper">
+    <HomeViewHeader />
+
+    <div class="overview">
+      <h2>Recipes</h2>
+      <RecipeGrid />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.content-wrapper {
+  display: flex;
+  flex-direction: column;
+  row-gap: 80px;
+  width: 100%;
+
+  .overview {
+    width: 100%;
+  }
+}
+</style>
