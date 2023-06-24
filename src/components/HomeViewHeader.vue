d374177a7ed558d26ef50f060541613178e99a4a
Marc Klose <marcklose99@gmail.com>
2023-06-24 08:54:01 +0200

    [REFACOTR] Changed and removed class names

--- src/components/HomeViewHeader.vue
+++ src/components/HomeViewHeader.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import SearchBarInput from "@/components/SearchBarInput.vue"
+import SearchBarInput from "@/components/SearchBarInput.vue";
 </script>
 
 <template>
   <header>
-    <div class="container searchbar">
-        <h1>Find the cheapest recipes</h1>
-        <SearchBarInput/>
-    </div>
+    <SearchBarInput />
   </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 header {
   position: relative;
   width: 100%;
@@ -21,38 +18,6 @@ header {
   background-repeat: no-repeat;
   background-position: 50% 85%;
 
-  .container.searchbar {
-    position: absolute;
-    bottom: -60px;
-    left: 50%;
-    transform: translateX(-50%);
-    height: 150px;
-    background-color: #fff;
-    box-shadow:
-        0px 0.9px 2.2px rgba(0, 0, 0, 0.02),
-        0px 2.2px 5.3px rgba(0, 0, 0, 0.028),
-        0px 4.1px 10px rgba(0, 0, 0, 0.035),
-        0px 7.4px 17.9px rgba(0, 0, 0, 0.042),
-        0px 13.8px 33.4px rgba(0, 0, 0, 0.05),
-        0px 33px 80px rgba(0, 0, 0, 0.07);
-    border-radius: 8px;
-  }
 
-  h1 {
-    z-index: 10;
-    margin: 20px 0 0;
-  }
-
-  .container.searchbar:before {
-    content: '';
-    position: absolute;
-    top: 8px;
-    left: 8px;
-    width: calc(100% - 16px);
-    height: calc(100% - 16px);
-    background-color:#A0C49D;
-    z-index: 1;
-    border-radius: 6px;
-  }
 }
 </style>
