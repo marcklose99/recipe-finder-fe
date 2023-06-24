a9e03cee070569ef6d06b0c27531fb40ffb875ff
Marc Klose <marcklose99@gmail.com>
2023-06-24 09:01:31 +0200

    [REFACTOR] Renamed classes and outsourced in components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,29 @@
 <script setup lang="ts">
-import RecipeGrid from "@/components/RecipeGrid.vue";
-import HomeViewHeader from "@/components/HomeViewHeader.vue";
-import AddPopup from "@/components/popup/AddPopup.vue";
-import { usePopupStore } from "@/stores/PopupStore";
-import AddCard from "@/components/cards/AddCard.vue";
-
+import MainHeader from "@/components/MainHeader.vue";
 </script>
 
 <template>
-  <!-- <AddPopup v-if="popupStore.isActive" /> -->
-  <main>
-    <HomeViewHeader />
-    <div class="container recipes">
-      <div class="heading">
-        <h2>Selbst hinzugefügte Rezepte</h2>
-        <AddCard />
+  <div class="container">
+    <MainHeader />
+    <main>
+      <div class="content-wrapper">
+        <RouterView />
       </div>
-
-      <RecipeGrid />
-    </div>
-  </main>
+    </main>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-main {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+<style lang="scss">
+.container {
+  width: 1300px;
+  // background-color: var(--main-section);
+  margin: 0 auto;
 
-.container.recipes {
-  padding-top: 100px;
-
-  .heading {
-    padding: 0px 20px;
-    width: 100%;
+  .content-wrapper {
     display: flex;
-    justify-content: flex-start;
-  }
-
-  h2 {
+    flex-direction: column;
+    row-gap: 80px;
     width: 100%;
-    text-align: left;
-    padding-left: 20px;
   }
 }
 </style>
