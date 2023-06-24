fc3426968c40b4f46d659a41bdc8f0992de295f3
Marc Klose <marcklose99@gmail.com>
2023-06-24 08:55:48 +0200

    [ADD] Main website header with logo and nav links

---
+++ src/components/LinkHeader.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="link-header">
+    <router-link class="link" to="/">Home</router-link>
+
+    <!-- <router-link class="link" to="/rezepte">Rezepte</router-link> -->
+
+    <router-link class="link" to="/create">Rezept hinzufügen</router-link>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.link-header {
+  display: flex;
+  justify-content: center;
+  margin-top: 12px;
+  .link {
+    text-align: center;
+    font-size: 21px;
+    line-height: 27px;
+    color: var(--nav-link);
+    font-weight: 400;
+    &:link {
+      text-decoration: none;
+    }
+  }
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import LinkHeader from "@/components/LinkHeader.vue";
+import LogoIcon from "@/components/icons/LogoIcon.vue";
+</script>
+
+<template>
+  <header>
+    <LogoIcon />
+
+    <LinkHeader />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/base.scss";
+header {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
