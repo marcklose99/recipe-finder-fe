1ccba8c0ad97b843fbc41f8ca50e95f226ad14ee
Marc Klose <marcklose99@gmail.com>
2023-06-24 08:58:42 +0200

    [ADD] Store keeps track of selectedFilter

--- src/stores/RecipeStore.ts
+++ src/stores/RecipeStore.ts
@@ -1,27 +1,33 @@
-import type {Ref} from 'vue'
-import {ref, watch} from 'vue'
-import {defineStore} from 'pinia'
-import type {IRecipe} from "@/data/IRecipe";
-import { usePopupStore } from './PopupStore';
+import type { Ref } from "vue";
+import { ref, watch } from "vue";
+import { defineStore } from "pinia";
+import type { IRecipe } from "@/data/IRecipe";
+import FetchService from "@/services/FetchService";
 
-export const useRecipeStore = defineStore('recipeStore', () => {
-  const popupStore = usePopupStore();
+export const useRecipeStore = defineStore("recipeStore", () => {
   const allRecipes: Ref<IRecipe[]> = ref([]);
   const filteredRecipes: Ref<IRecipe[]> = ref([]);
+
+  const selectedFilter: Ref<string> = ref("");
 
   // set counter to zero if a new recipe was added
   watch(allRecipes, () => {
     // popupStore.counter = 0;
   });
-  
-  function sortRecipe(searchVal: string): void {
-    
-    if(searchVal != "") {
-      filteredRecipes.value =  filteredRecipes.value.filter((val: IRecipe) => val.title.toLowerCase().includes(searchVal.toLowerCase()));
 
+  watch(selectedFilter, async () => {
+    allRecipes.value = await FetchService.fetchRecipe(selectedFilter.value);
+    filteredRecipes.value = allRecipes.value;
+  });
+
+  function sortRecipe(searchVal: string): void {
+    if (searchVal != "") {
+      filteredRecipes.value = filteredRecipes.value.filter((val: IRecipe) =>
+        val.title.toLowerCase().includes(searchVal.toLowerCase())
+      );
     } else {
       filteredRecipes.value = allRecipes.value;
     }
   }
-  return { allRecipes, filteredRecipes, sortRecipe }
-})
+  return { selectedFilter, allRecipes, filteredRecipes, sortRecipe };
+});
