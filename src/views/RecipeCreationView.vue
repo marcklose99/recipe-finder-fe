e74d5f5894e976b3fb107951337ac52b8cf37d99
Marc Klose <marcklose99@gmail.com>
2023-06-24 09:01:54 +0200

    [REFACTOR] Change structur of component

--- src/views/RecipeCreationView.vue
+++ src/views/RecipeCreationView.vue
@@ -1,124 +1,18 @@
 <script setup lang="ts">
-import router from "@/router";
-import { computed, ref } from "vue";
-import type { Ref } from "vue";
-import ArrowDown from "@/components/icons/ArrowDown.vue";
-import { useRecipeCreationStore } from "@/stores/RecipeCreationStore";
-import IngredientSelection from "@/components/popup/IngredientSelection.vue";
-
-const recipeCreationStore = useRecipeCreationStore();
-const show = ref(false);
-const counter: Ref<number> = ref(0);
-const views = [
-    {
-        name: "ingredient-creation"
-    },
-    {
-        name: "instruction-creation"
-    }
-]
-
-
-function next() {
-    if (recipeCreationStore.recipe.ingredientList.length != 0) {
-        recipeCreationStore.ingredientSelectionCompleted = true;
-    }
-    if (recipeCreationStore.enabled) {
-        console.log("Hello")
-        show.value = true;
-        router.push({
-            name: views[counter.value].name,
-            replace: true
-        });
-        counter.value++;
-    } else {
-        // bind class with error msg
-    }
-}
-function showContext() {
-    recipeCreationStore.ingredientSelectionCompleted = false;
-    router.push({ name: 'ingredient-creation' })
-}
+import RecipeHeader from "@/components/RecipeHeader.vue";
+import IngredientCreation from "@/components/popup/IngredientCreation.vue";
+import InstructionCreation from "@/components/InstructionCreation.vue";
+import RecipeFooter from "@/components/RecipeFooter.vue";
 </script>
+
 <template>
-    <main>
-        <div class="content-wrapper">
-            <div class="content">
-                <h2>Erstellen Sie ihr Rezept!</h2>
-                <div class="description">
-                    <label for="recipeName">
-                        <span>Name vom Rezept</span>
-                        <input class="standard-input" v-model="recipeCreationStore.recipe.title" id="recipeName" />
-                    </label>
-                </div>
-                <div class="selected-ingredients" v-if="recipeCreationStore.ingredientSelectionCompleted">
-                    <IngredientSelection @click="showContext()" />
-                </div>
-                <section v-if="show" class="active-section">
-                    <RouterView />
-                </section>
-                <ArrowDown class="next-btn" :class="{ jump: recipeCreationStore.enabled }" @click="next()" />
-            </div>
-        </div>
-    </main>
+  <RecipeHeader />
+  <IngredientCreation />
+  <InstructionCreation />
+  <RecipeFooter />
+  
 </template>
-<style lang="scss">
+
+<style lang="scss" scoped>
 @import "@/assets/base.scss";
-
-main {
-    .content-wrapper {
-        min-height: 100vh;
-        width: 70%;
-        margin: 0 auto;
-        background-color: #F3F5F8;
-
-        .content {
-            width: 80%;
-            margin: 0 auto;
-            display: flex;
-            flex-direction: column;
-
-            h2 {
-                margin-bottom: 30px;
-            }
-
-            .next-btn {
-                margin-top: 30px;
-                align-self: center;
-                width: 50px;
-                height: 50px;
-                cursor: pointer;
-            }
-
-            .active-section {
-                padding: 10px;
-                min-height: 200px;
-                height: fit-content;
-                background-color: #DDE0E7;
-            }
-        }
-
-        .jump {
-            animation: jump 3s infinite ease-in;
-        }
-
-        @keyframes jump {
-            0% {
-                transform: translate(0, 0)
-            }
-
-            10% {
-                transform: translate(0, 2px)
-            }
-
-            30% {
-                transform: translate(0, 4px)
-            }
-
-            90% {
-                transform: translate(0, 2px);
-            }
-        }
-    }
-}
 </style>
